6fda1352cc447fa84f1e04f7c77c06d090b7063f
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-03-10 09:56:50 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -96,4 +96,6 @@ async def main():
     await app.run_polling()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
