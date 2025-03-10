bad07e0161dd324c13f12016b39b69b2c07689a7
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-03-10 14:38:58 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -37,4 +37,6 @@ if __name__ == '__main__':
         nest_asyncio.apply()
     except ImportError:
         pass
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
